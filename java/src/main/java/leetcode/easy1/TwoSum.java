859fdb699e757a533b7e9bbd41578f3f4429ad1f
areph <40balmung@gmail.com>
2021-03-15 18:26:37 +0900

    add comment

--- java/src/main/java/leetcode/easy1/TwoSum.java
+++ java/src/main/java/leetcode/easy1/TwoSum.java
@@ -11,12 +11,19 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // ２つの数を加算してtargetの値になる添字を検索する
+
+        // ハッシュに格納しつつ検索する
         Map<Integer, Integer> hash = new HashMap<>();
+        // 配列の最初から順番に検索していく
         for (int i = 0; i < nums.length; i++) {
+            // targetから数値を差し引いて差分を取得
             int key = target - nums[i];
+            // 差分の値がすでにハッシュにある場合は現在の添字とハッシュに格納した添字が答え
             if (hash.containsKey(key)) {
                 return new int[] { hash.get(key), i };
             }
+            // ハッシュに存在しない場合は数字と添字をハッシュへ格納する
             hash.put(nums[i], i);
         }
         throw new IllegalArgumentException("invalid arguments");
